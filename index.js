93952ff3fec13bf26f9ac45f511028d829df304c
ZQ Yeo <zhiquan.yeo@gmail.com>
2017-11-21 12:07:19 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const rosnodejs = require('rosnodejs');
+const std_msgs = rosnodejs.require('std_msgs').msg;
+
+function listener() {
+    rosnodejs.initNode('/listener_node')
+        .then((rosNode) => {
+            let sub = rosNode.subscribe('/chatter', std_msgs.String,
+            (data) => {
+                rosnodejs.log.info('I heard: [' + data.data + ']');
+            });
+        });
+}
+
+if (require.main === module) {
+    listener();
+}
